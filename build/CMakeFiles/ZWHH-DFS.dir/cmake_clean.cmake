dc8d3bb4b85f8089127a6b5e3118b96d6fa1b3e5
hjh9730 <973073525@qq.com>
2022-08-07 22:33:47 +0800

    HJH Commit

---
+++ build/CMakeFiles/ZWHH-DFS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/ZWHH-DFS"
+  "../bin/ZWHH-DFS.pdb"
+  "CMakeFiles/ZWHH-DFS.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ZWHH-DFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
